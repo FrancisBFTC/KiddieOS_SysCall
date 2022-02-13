0f8a0210c57d5f7e3c17b9bae4166eb25b2b76d5
FrancisBFTC <bftcorporations@gmail.com>
2022-02-13 20:31:15 -0300

    Executável KiddieOS Device Manager
    O intuito deste sistema é apresentar informações de dispositivos para o usuário e tornar facilmente manipulável a instalação de Drivers e gerenciamento de dispositivos. O código simples que se encontra neste arquivo é responsável por ter 3 loops internos: O 1ª que incrementa 255 vezes, o 2ª que incrementa 32 vezes e o 3ª que incremente 8 vezes, totalizando 8192 iterações para leitura de dispositivos PCI. A cada iteração um dispositivo é lido e verificado. A missão inicial deste executável é exibir uma árvore hierárquica dos dispositivos, no entanto contém alguns problemas a procura de solução.

--- devmgr.asm
+++ devmgr.asm
@@ -1,27 +1,103 @@
 %INCLUDE 	"libasm.inc"
 
+VCL   db 0,0
+bus   db 0
+dev   db 0
+fun   db 0
+count dw 0
+ct 	  dw 0
+
+Spaces db 0x0D,"|___",0
+
+Verify_Class:
+	pushad
+	mov 	eax, 0x1B
+	mov 	bh, [bus]
+	mov 	bl, [dev]
+	mov 	cl, [fun]
+	int 	0xCE
+	cmp 	ax, 0xFFFF
+	je 		Ret_Verify
+	mov		bx, word[ct]
+	cmp		word[count], bx
+	ja 		CompareToSub
+	
+	mov 	bx, word[count]
+	mov 	edi, VCL
+	mov 	byte[edi + ebx], ah
+			
+	mov 	eax, 0x16
+	mov 	bh, [bus]
+	mov 	bl, [dev]
+	mov 	cl, [fun]
+	int 	0xCE
+	mov 	eax, 0x01
+	mov  	edx, 0x07
+	int 	0xCE
+	
+	inc 	word[count]
+	jmp 	Ret_Verify
+	
+CompareToSub:
+	cmp 	byte[VCL], ah
+	jne 	Ret_Verify
+	
+	mov 	eax, 0x01
+	mov 	esi, Spaces
+	mov  	edx, 0x0F
+	int 	0xCE
+
+	mov 	eax, 0x17
+	mov 	bh, [bus]
+	mov 	bl, [dev]
+	mov 	cl, [fun]
+	int 	0xCE
+	mov 	eax, 0x01
+	mov  	edx, 0x06
+	int 	0xCE
+	
+Ret_Verify:
+	popad
+	ret
+
 Main(ARGC, ARGV)
-	; VirtualBox Graphics Adapter
-	mov 	eax, 0x4    ; SysCall PCI Get_Name_Device
-	mov 	bh, 0       ; Bus Number
-	mov 	bl, 2       ; Device Number
-	mov 	cl, 0       ; Function Number
-	int 	0xCE        ; Invoke the Syscall
-						; Return = ESI
-	                    ; ESI = Device String; 
-	mov 	eax, 0x1    ; SysCall Print_Zero_Terminated
-	mov 	edx, 0x05   ; Color Back|Fore
+	; Load Driver and Initialize Device
+	mov 	eax, 0x14   ; Syscall Init_Device
 	int 	0xCE        ; Invoke the Syscall
 	
-	; PIIX4 ACPI
-	mov 	eax, 0x4    ; SysCall PCI Get_Name_Device
-	mov 	bh, 0       ; Bus Number
-	mov 	bl, 7       ; Device Number
-	mov 	cl, 0       ; Function Number
-	int 	0xCE        ; Invoke the Syscall
-						; Return = ESI
-	                    ; ESI = Device String; 
-	mov 	eax, 0x1    ; SysCall Print_Zero_Terminated
-	mov 	edx, 0x02   ; Color Back|Fore
+	xor 	ebx, ebx
+	
+	mov 	ecx, 255
+Loop_Bus:
+	push 	ecx
+	mov 	byte[dev], 0
+	
+	mov 	ecx, 32
+	Loop_Dev:
+		push 	ecx
+		mov 	byte[fun],0
+		
+		mov 	ecx, 8
+		Loop_Func:
+			push 	ecx
+			
+			call 	Verify_Class
+			
+			inc 	byte[fun]
+			pop 	ecx
+			loop 	Loop_Func
+		
+		inc 	byte[dev]
+		pop 	ecx
+		loop 	Loop_Dev
+	
+	inc 	byte[bus]
+	pop 	ecx
+	loop 	Loop_Bus
+	
+	inc 	word[ct]
+	
+	; Close Driver
+	mov 	eax, 0x15   ; Syscall Close_Device
 	int 	0xCE        ; Invoke the Syscall
 .EndMain
